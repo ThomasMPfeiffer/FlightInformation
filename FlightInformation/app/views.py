1eeba09cdb350b01b14452d7db4149dab95a7611
Thomas Pfeiffer <ThomasMPfeiffer@web.de>
2022-01-20 17:45:23 +0100

    Offersearch request

--- FlightInformation/app/views.py
+++ FlightInformation/app/views.py
@@ -6,18 +6,48 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from django.http import HttpResponse
+from amadeus import Client, ResponseError 
+from django.contrib import messages 
+
+amadeus = Client(client_id='In8XtAGXlpWyURmQE3FQFSyGiUC3YSAL', 
+                 client_secret='xwDB70qfB4AAnMrT', 
+                 log_level='debug') 
 
 def home(request):
-    """Renders the home page."""
-    assert isinstance(request, HttpRequest)
-    return render(
-        request,
-        'app/index.html',
-        {
-            'title':'Home Page',
-            'year':datetime.now().year,
-        }
-    )
+    kwargs = {'originLocationCode': request.POST.get('Origin'), 
+              'destinationLocationCode': request.POST.get('Destination'), 
+              'departureDate': request.POST.get('Departuredate'), 
+              'returnDate': request.POST.get('Returndate')} 
+    try: 
+        purpose = amadeus.travel.predictions.trip_purpose.get( 
+            **kwargs).data['result'] 
+    except ResponseError as error: 
+        print(error) 
+        messages.add_message(request, messages.ERROR, error) 
+        return render(request, 'app/index.html', {}) 
+    return render(request, 'app/index.html', {'prediction': purpose})    
+
+
+
+def offersearch(request): 
+    kwargs = {'originLocationCode': request.POST.get('Origin'), 
+            'destinationLocationCode': request.POST.get('Destination'), 
+            'departureDate': request.POST.get('Departuredate'), 
+            'adults':  request.POST.get('Amount_adults')}
+            
+    try: 
+        response = amadeus.shopping.flight_offers_search.get(
+        **kwargs)
+        print(response.data)
+    except ResponseError as error: 
+        print(error) 
+        messages.add_message(request, messages.ERROR, error) 
+        return render(request, 'app/offersearch.html', {}) 
+    return render(request, "app/offersearch.html", {"offersearch": response})
+
+
+
 
 def contact(request):
     """Renders the contact page."""
@@ -32,6 +62,9 @@ def contact(request):
         }
     )
 
+
+
+
 def about(request):
     """Renders the about page."""
     assert isinstance(request, HttpRequest)
@@ -44,3 +77,13 @@ def about(request):
             'year':datetime.now().year,
         }
     )
+
+
+
+
+
+
+
+
+
+
